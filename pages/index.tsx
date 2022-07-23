d90a2f9a66212ac779da24d2a54b586a4ab4463b
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-07-23 16:56:34 +0600

    added Button component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
-import type { NextPage } from 'next'
-import { Htag } from '../components'
-
-
+import type { NextPage } from "next";
+import { Button, Htag } from "../components";
 
 const Home: NextPage = () => {
   return (
-   <div>
-    <Htag tag="h1">Заголовок</Htag>
-   </div>
-   
-  )
-}
+    <div>
+      <Htag tag="h1">Заголовок</Htag>
+      <Button appearance="primary">Узнать подробнее</Button>
+      <Button appearance="ghost">Читать отзывы</Button>
+      <Button appearance="ghost">Читать отзывы</Button>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
