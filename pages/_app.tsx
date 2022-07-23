ff4497d0a86ca716ed993253416807a05998c1cf
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-07-23 12:54:17 +0600

    start project, added fonts and color styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>MyTop - наш лучший топ</title>
+        <meta name="description" content="My Top by Adilet Berdibekov app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
