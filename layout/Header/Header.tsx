b89f25ac5ba0739801e89b9046d8c33484158289
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-07-24 23:17:49 +0600

    added style to Layout component

--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { HeaderProps } from "./Header.props";
-import styles from "./Sidebar.module.css";
+import styles from "./Header.module.css";
 import cn from "classnames";
 
 export const Header = ({ ...props }: HeaderProps): JSX.Element => {
--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -3,19 +3,19 @@ import { LayoutProps } from "./Layout.props";
 import { Sidebar } from "./Sidebar/Sidebar";
 import { Header } from "./Header/Header";
 import { Footer } from "./Footer/Footer";
-import styles from "./P.module.css";
+import styles from "./Layout.module.css";
 import cn from "classnames";
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
-    <>
-      <Header />
-      <div>
-        <Sidebar />
-        <div>{children}</div>
-      </div>
-      <Footer />
-    </>
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body}>
+          {children}
+        </div>
+      <Footer className={styles.footer} />
+    </div>
   );
 };
 
